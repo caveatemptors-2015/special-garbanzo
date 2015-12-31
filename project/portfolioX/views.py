2cc9bdfc0b37510d79139334d2f883c319658e4f
Cody Hess <cody.hess@gmail.com>
2015-12-31 17:20:47 -0500

    Implement routing to barebones templates.
    / renders users/templates/users/welcome.html
    /portfolioX renders portfolioX/templates/portfolioX/holdings.html
    /portfolioX/buysell renders portfolioX/templates/portfolioX/buysell.html
    /portfolioX/addremove renders portfolioX/templates/portfolioX/addremove.html
    /quotes renders quotes/templates/quotes/analytics.html

--- project/portfolioX/views.py
+++ project/portfolioX/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def buysell(request):
+	return render(request, 'portfolioX/buysell.html')
+
+def addremove(request):
+	return render(request, 'portfolioX/addremove.html')
+
+def holdings(request):
+	return render(request, 'portfolioX/holdings.html')
--- project/quotes/views.py
+++ project/quotes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def analytics(request, ticker_symbol=None):
+	return render(request, 'quotes/analytics.html')
