20bfce9790777a31940fc3c37056f1c38cd4e549
mohamed-aziz-bennour <medaziz.bennour@gmail.com>
2016-01-04 11:40:43 -0500

    creation of  user form page

--- project/users/views.py
+++ project/users/views.py
@@ -6,36 +6,40 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def welcome(request):
-	return render(request, 'users/welcome.html', {'form': {'login':
-		UserForm(), 'register': UserForm()}})
+    return render(request, 'users/welcome.html', {'form': {'login':
+        UserForm(), 'register': UserForm()}})
 
 def login_(request):
-	if request.method == 'POST':
-		user_form = UserForm(request.POST)
-		username = user_form['username'].value()
-		password = user_form['password'].value()
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				request.session['user_id'] = username
-				return redirect('users:welcome')
-		return render(request, 'users/welcome.html', {'form':
-			{'login': user_form, 'register': UserForm()}} )
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        username = user_form['username'].value()
+        password = user_form['password'].value()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                request.session['user_id'] = username
+                return redirect('users:welcome')
+        return render(request, 'users/welcome.html', {'form':
+            {'login': user_form, 'register': UserForm()}} )
+
+
+def create_user(request):
+    return render(request, 'users/create_user.html', {'form':UserForm()})
 
 def register(request):
-	if request.method == 'POST':
-		user_form = UserForm(request.POST)
-		if user_form.is_valid():
-			user = user_form.save(commit=False)
-			raw_pass = user.password
-			user.password = make_password(user.password)
-			user.save()
-			user = authenticate(username=user.username, password=raw_pass)
-			if user is not None:
-				if user.is_active:
-					request.session['user_id'] = username
-					login(request, user)
-					return redirect('users:welcome')
-		return render(request, 'users/welcome.html', {'form':
-			{'login': UserForm(), 'register': user_form}} )
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save(commit=False)
+            raw_pass = user.password
+            user.password = make_password(user.password)
+            user.save()
+            user = authenticate(username=user.username, password=raw_pass)
+            if user is not None:
+                if user.is_active:
+                    request.session['user_id'] = user.username
+                    login(request, user)
+                    return redirect('users:welcome')
+        return render(request, 'users/welcome.html', {'form':
+            {'login': UserForm(), 'register': user_form}} )
