964c275e4a06eb209b7d90d7d0ea1b31d3fb54db
ARTYUDIN <INVESTHOLD2015@YAHOO.com>
2016-01-09 15:33:20 -0500

    did correct forms for our model

--- project/portfolioX/urls.py
+++ project/portfolioX/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.HoldingView.as_view(), name='holdings'),
+	url(r'^createportfolio', views.UserCreatePortfolio.as_view(), name='create_portfolio'),
 	url(r'^buysell/', views.BuySellView.as_view(), name='buysell'),
 	url(r'^addremove/', views.AddRemoveView.as_view(), name='addremove'),
 	
